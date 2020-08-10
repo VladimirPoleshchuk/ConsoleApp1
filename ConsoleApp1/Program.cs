d0e0f840825b727552b77d4a918381eadf915127
Vladimir Poleshchuk <polvlad1m@yandex.ru>
2020-08-10 21:50:36 +0700

    Добавил метод

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,18 +8,62 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+       static void MyMethod()
         {
-            int [,] table = new int [3,4];
+        int [,] table = new int [3,4];
             for (int i = 0; i <3; i++)
 			{
                 for (int j = 0; j <4; j++)
 			{
                 table[i,j]=(i*4)+j+1;
-                    Console.WriteLine(table[i,j]+ " ");
+                    Console.Write(table[i,j]+ " ");
 			}
 			}
-           
+           Console.WriteLine();
+            int [] nams = new int[10];
+            for (int i = 0; i < nams.Length; i++)
+			{
+                nams[i]=i*i;
+                Console.Write(nams[i]+" ");
+			}
+            Console.WriteLine();
         }
+        static void MyMethod1()
+        {
+        int i , j;
+            int[] nums1 = new int [10];
+            int[] nums2 = new int [10];
+
+            for ( i = 0; i <nams1.Length; i++)
+			{
+              nums1[i]=i;
+			}
+            Console.WriteLine("Исходное содержимое массива: ");
+
+            for ( i = 0; i <nums1.length; i++)
+			{
+                Console.Write(nums1[i] + " ");
+			}
+             if (nums1.Length==nums2.Length)
+         	{
+                for (i = 0, j=nums1.Length-1; i < nums1.Length; i++, j--)
+			{
+              nums2[j]=nums1[i];
+			}
+              Console.WriteLine("Содержимое массива в обратном порядке: ");
+                for ( i = 0; i <nums2.length; i++)
+			{
+                    Console.WriteLine(nums2[i] + " ");
+			}
+                Console.WriteLine();
+	        }
+
+        }
+        static void Main(string[] args)
+        {
+        // MyMethod();  
+           MyMethod1();
+        }
+        
     }
 }
