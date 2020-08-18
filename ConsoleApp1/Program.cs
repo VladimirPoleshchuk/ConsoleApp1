8f299fad2702d8fdf51b3695215ff3fcfdabd6e4
Vladimir Poleshchuk <polvlad1m@yandex.ru>
2020-08-18 22:20:15 +0700

    new Methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,78 +6,41 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class ConvertDigitsToWords
     {
-       static void MyMethod()
+        static void Main()
         {
-        int [,] table = new int [3,4];
-            for (int i = 0; i <3; i++)
-			{
-                for (int j = 0; j <4; j++)
-			{
-                table[i,j]=(i*4)+j+1;
-                    Console.Write(table[i,j]+ " ");
-			}
-			}
-           Console.WriteLine();
-            int [] nams = new int[10];
-            for (int i = 0; i < nams.Length; i++)
-			{
-                nams[i]=i*i;
-                Console.Write(nams[i]+" ");
-			}
-            Console.WriteLine();
-        }
+            int num;
+            int nextdigit=0;
+            int numdigits=0;
+            int[] n = new int[4];
 
+            string[] digits = { "ноль","один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
 
+            num = 1908;
 
-        static void MyMethod1()
-        {
-             int i,j;
-            int [] nums1 = new int [10];
-            int [] nums2 = new int [10];
+            Console.WriteLine($"Число: {num}");
 
-            for ( i = 0; i <nums1.Length; i++) nums1[i]=i;
-			
-            Console.WriteLine("Исходное содержимое массива: ");
-        
-            for (  i = 0; i <nums1.Length; i++)
-			{
-                Console.Write(nums1[i] + " ");
-			}
-             if (nums1.Length==nums2.Length)
-         	{
-                for ( i = 0, j=nums1.Length-1; i < nums1.Length; i++, j--)
-			{
-              nums2[j]=nums1[i];
-			}
-                MethodNams2();
+            Console.WriteLine("Число словами: ");
 
-                Console.WriteLine();
-	        }
-          else
-	      {
-                MethodNams2();
-
-                Console.WriteLine("\nМассивы не соответствуют по размеру!");
-	       }
-       void MethodNams2()
+            do
             {
-                Console.WriteLine("\nСодержимое массива в обратном порядке: ");
+                nextdigit = num % 10;
+                n[numdigits] = nextdigit;
+                numdigits++;
+                num = num/ 10;
+            } while (num>0);
+            numdigits--;
+            for (; numdigits>=0; numdigits--)
+                Console.Write(digits[n[numdigits]]+ " ");
+            Console.WriteLine();
+            
+            //Проверка метода
+            string str = "Привет кто сегодня пойдет?";
 
-                for ( i = 0; i <nums2.Length; i++)
-			    {
-                    Console.Write(nums2[i] + " ");
-			    }
-            }
+             str = str.Substring( 5,  10);
+            Console.WriteLine(str);
 
-
-        }
-        static void Main(string[] args)
-        {
-        // MyMethod();  
-           MyMethod1();
-         
         }
         
     }
---
+++ MethodsAndClasses/Program.cs
@@ -0,0 +1,135 @@
+﻿
+using System;
+using System.Text;
+
+namespace MethodsAndClasses
+{
+    class Program
+    {
+        #region MyREF
+        /*
+         static void Foo(int p)
+        {
+            p += 1;
+            Console.WriteLine(p);
+         }
+        static void Foo(StringBuilder fooSB)
+        {
+            fooSB.Append("test");
+            fooSB = null;
+        }
+        static void Foo(ref int p)
+        {
+            p += 1;
+            Console.WriteLine(p);
+        }
+        static void Swap(ref string a, ref string b)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }*/
+        #endregion
+
+        #region Out
+        /* static void Split(string name, out  string firstName, out string lastName )
+      {
+          int i = name.LastIndexOf(' ');
+          firstName = name.Substring(0, i);
+          lastName = name.Substring(i + 1);
+      }
+        */
+        #endregion
+
+        static void Addition(params int[] integers)
+        {
+            int result = 0;
+            for (int i = 0; i <integers.Length; i++)
+            {
+                result += integers[i];
+                Console.WriteLine(result);
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("******************");
+        }
+        
+        static void AdditionMas(int[] integers, int k)
+        { 
+            int result = 0;
+            for (int i = 0; i <integers.Length; i++)
+            {
+                result += (integers[i] * k);
+                Console.WriteLine(result);
+            }
+            Console.WriteLine(result);
+            
+        }
+
+
+
+
+        public static void Main()
+        {
+            #region REF
+            /*
+            int x = 8;
+            Foo(x);
+            Console.WriteLine(x);
+
+            StringBuilder sb = new StringBuilder();
+            Foo(sb);
+            Console.WriteLine(sb.ToString());
+
+            int z = 8;
+            Foo(ref z);
+            Console.WriteLine(z);
+
+            string name = "Penn";
+            string name2 = "Teller";
+            Console.WriteLine("{0} {1}", name, name2); 
+            Swap(ref name, ref name2);
+            Console.WriteLine("{0} {1}", name, name2);
+            */
+            #endregion
+            #region Out
+            /*
+            string c = "Stevie Ray Vaughan";
+            Split(c, out string a, out _);
+            Console.WriteLine(a);
+            Console.WriteLine();
+            */
+            #endregion
+            #region Displey
+            /*  Displey("Tom", 24);
+           Displey("Mery", 18);
+
+           int a = Convert.ToInt32(Console.ReadLine());
+           int b = Convert.ToInt32(Console.ReadLine());
+
+           int sum =Sum(a, b);
+           Console.WriteLine(sum);
+
+
+       }
+           static void Displey(string name, int age)
+           {
+           Console.WriteLine($"Name: {name}, Age: {age}");
+           }
+       static int Sum(int x, int y)
+       {
+           return x + y;
+       }*/
+            #endregion
+
+            Addition(1, 2, 3, 4);
+
+            int[] array = { 1, 2, 3, 4 };
+            AdditionMas(array, 2);
+
+           
+
+
+        }
+       
+        }
+    }
